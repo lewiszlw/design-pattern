d9abe4ec12c542cd60bc157ad416a17fb0b949c3
zhanglinwei02 <zhanglinwei02@meituan.com>
2018-11-19 11:46:14 +0800

    单例模式-枚举实现

---
+++ src/main/java/tech/lewiszlw/singletonpattern/Singleton6.java
@@ -0,0 +1,15 @@
+package tech.lewiszlw.singletonpattern;
+
+/**
+ * Desc:
+ *
+ * @author zhanglinwei02
+ * @date 2018/11/19
+ */
+public enum Singleton6 {
+    INSTANCE;
+
+    public void method() {
+        System.out.println("对象方法");
+    }
+}

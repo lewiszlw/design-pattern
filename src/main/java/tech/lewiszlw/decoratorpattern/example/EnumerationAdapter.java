d4c7b5c13eb2508aba1e65e53f8f489befe2a152
zhanglinwei02 <zhanglinwei02@meituan.com>
2018-11-19 12:20:23 +0800

    适配器模式-示例

---
+++ src/main/java/tech/lewiszlw/decoratorpattern/example/EnumerationAdapter.java
@@ -0,0 +1,26 @@
+package tech.lewiszlw.decoratorpattern.example;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+
+/**
+ * Desc:
+ *
+ * @author zhanglinwei02
+ * @date 2018/11/19
+ */
+public class EnumerationAdapter implements Enumeration {
+    private Iterator iterator;
+
+    public EnumerationAdapter(Iterator iterator) {
+        this.iterator = iterator;
+    }
+
+    @Override public boolean hasMoreElements() {
+        return iterator.hasNext();
+    }
+
+    @Override public Object nextElement() {
+        return iterator.next();
+    }
+}
